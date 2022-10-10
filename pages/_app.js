23791e5b9f06e80d6c7a6379d22ac60bbfc25b4a
Herb-Wright <herb.e.wright@gmail.com>
2022-10-09 20:51:26 -0600

    fixed ResponsiveDiv for prod

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,14 @@ import '../styles/globals.scss';
 import '@fontsource/open-sans/300.css'
 import '@fontsource/open-sans/400.css'
 import '@fontsource/open-sans/600.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			</Head>
 			<Navbar
 				links={[
 					{ label: 'Home', path: '/' },
