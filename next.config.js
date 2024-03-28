a9755d269970bd70fad80bf54f17d9dfed66e97c
Herbie Wright <herb.e.wright@gmail.com>
2024-03-28 13:44:15 -0600

    updated packages and added workflow

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	output: 'standalone',
+	output: isGithubActions ? 'export' : 'standalone',
 }
 
 module.exports = nextConfig
